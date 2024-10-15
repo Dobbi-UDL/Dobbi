d9ac0f67d734af9fcc4eb80d688a4ef587ea5ff9
Mihaela <mbuturuga@gmail.com>
2024-10-15 12:01:11 +0200

    updated homepgae navbar logic

--- dobbi-web/src/app/page.js
+++ dobbi-web/src/app/page.js
@@ -1,16 +1,22 @@
+"use client";
+
 import Header from "@/components/common/Header";
 import Navbar from "@/components/common/Navbar";
 import Hero from "@/components/homepage/Hero";
 import Features from "@/components/homepage/Features";
 import CTA from "@/components/homepage/CTA";
 import Footer from "@/components/common/Footer";
+import { useAuth } from "@/contexts/AuthContext";
+
 
 const BG_COLOR = '#FFFCF9';
 
 export default function Home() {
+  const { user } = useAuth();
+
   return (
     <div className="min-h-screen flex flex-col">
-      {isLoggedIn ? <Navbar /> : <Header />}
+      {user ? <Navbar /> : <Header />}
       <main className="flex-grow">
         <Hero />
         <Features />
