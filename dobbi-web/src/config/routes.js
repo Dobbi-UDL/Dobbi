30f03be8297f1e54fa4c706128452e8e9927fe5e
Eric López <lopezs.eric@gmail.com>
2024-10-16 19:08:13 +0200

    working on register request

--- dobbi-web/src/config/routes.js
+++ dobbi-web/src/config/routes.js
@@ -2,10 +2,12 @@ const routes = {
     home: "/",
     login: "/login",
     register: "/register",
+    register_request: "/register-request",
     forgotPassword: "/forgot-password",
     dashboard: "/dashboard",
     adminDashboard: "/admin",
     downloads: "/downloads",
+    
     
 };
 
