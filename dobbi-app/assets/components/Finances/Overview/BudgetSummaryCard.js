66f06777a1049dbbf18294f46bd39a06f99945fe
Mihaela <mbuturuga@gmail.com>
2024-11-08 00:39:34 +0100

    Create BudgetSummary Card with static data

--- dobbi-app/assets/components/Finances/Overview/BudgetSummaryCard.js
+++ dobbi-app/assets/components/Finances/Overview/BudgetSummaryCard.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Text } from 'react-native';
 import Card from '../../common/Card';
 
+const mockData = {
+    totalIncome: 2000,
+    totalExpenses: 1000,
+    remainingBalance: 1000,
+};
 const BudgetSummaryCard = () => {
     return (
-        <Card title="Budget Summary"
-            cardStyle={{ width: 300 }}
-            titleStyle={{ fontSize: 18 }}>
-            <Text>Card content</Text>
+        <Card title="Summary">
+            <Text>Remaining balance: ${mockData.remainingBalance}</Text>
+            <Text>Total Income: ${mockData.totalIncome}</Text>	
+            <Text>Total Expenses: ${mockData.totalExpenses}</Text>
         </Card>
     );
 };
