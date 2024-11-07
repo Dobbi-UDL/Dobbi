9c4b6fc76194d5ee7e3d9291bf22dbc9ef311949
Mihaela <mbuturuga@gmail.com>
2024-11-07 14:08:35 +0100

    Create basic Card component

---
+++ dobbi-app/assets/components/common/Card.js
@@ -0,0 +1,30 @@
+// Card.js
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Card = ({ children }) => {
+    return (
+        <View style={styles.card}>
+            {children}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 7,
+        padding: 16,
+        margin: 8,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 2, // for Android shadow
+    }
+});
+
+export default Card;
