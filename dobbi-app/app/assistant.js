c9b3b1c02b08e659fe79f382c8aca91082b07e98
Mihaela <mbuturuga@gmail.com>
2024-11-18 23:05:47 +0100

    Add Header and NavBar to all screens

--- dobbi-app/app/assistant.js
+++ dobbi-app/app/assistant.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Header from '../assets/components/Header/Header';
+import { BottomNavBar } from '../assets/components/Navigation/BottomNavBar';
 
 const HelloPage = () => {
     return (
+        <>
+        <Header />
         <View style={styles.container}>
             <Text style={styles.text}>Hello, welcome to Dobbi App!</Text>
         </View>
+        <BottomNavBar />
+        </>
     );
 };
 
